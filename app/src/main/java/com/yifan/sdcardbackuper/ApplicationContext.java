e3aefbfbbf33d1cb1d5b520071c05e80d15e2fc4
kelvinsail <kelvinsail@163.com>
2019-07-02 13:55:48 +0800

    更新as插件版本

--- app/src/main/java/com/yifan/sdcardbackuper/ApplicationContext.java
+++ app/src/main/java/com/yifan/sdcardbackuper/ApplicationContext.java
@@ -3,6 +3,7 @@ package com.yifan.sdcardbackuper;
 import android.app.Application;
 import android.os.Environment;
 
+import com.squareup.leakcanary.LeakCanary;
 import com.yifan.utils.base.BaseApplication;
 
 import java.io.File;
@@ -25,5 +26,14 @@ public class ApplicationContext extends BaseApplication {
     public void onCreate() {
         super.onCreate();
         mInstances = this;
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        // Normal app init code...
+
     }
 }
